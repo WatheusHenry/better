c250354605a7a2e9cb0ecc927c1281e38fa76cbe
Matheus Henry <matheus.sanches@tray.net.br>
2025-08-08 14:28:55 -0300

    fix: atualizar configuração de reescritas no Vercel e adicionar suporte a ícones maskable no Vite

--- vite.config.ts
+++ vite.config.ts
@@ -23,8 +23,8 @@ export default defineConfig({
         display: 'standalone',
         start_url: '/',
         icons: [
-          { src: '/favicon.png', sizes: '192x192', type: 'image/png' },
-          { src: '/favicon.png', sizes: '512x512', type: 'image/png' }
+          { src: '/favicon.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
+          { src: '/favicon.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
         ]
       }
     })
