e5bb381f1a8e37a6eb569655d454a9d7840be038
Matheus Henry <goncalvessanches.mh@gmail.com>
2025-08-08 14:01:58 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Better',
+  webDir: 'dist'
+};
+
+export default config;
